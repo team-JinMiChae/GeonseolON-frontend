3e69403d3abc5f018c017e3e367d36618a558dbd
LeeJinyoung218 <amy021899@gmail.com>
2025-04-19 01:51:56 +0900

    ✨ Feat: 기본 레이아웃 및 라우팅 설정, 헤더 및 메인 페이지 컴포넌트 추가

---
+++ src/components/feature/Layout/Header.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+import styles from './Header.module.css';
+const Header = () => {
+    return (
+        <div className={styles.wrapper}>
+            Header
+        </div>
+    );
+};
+
+export default Header;
