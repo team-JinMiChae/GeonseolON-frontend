dcc5aabebcf311256a5f3fbb7146e86a0a78dd04
SemiHyeon <106103995+SemiHyeon@users.noreply.github.com>
2025-05-13 05:58:15 +0900

    메인페이지 레이아웃 리팩토링 + 카드뉴스 컴포넌트 생성(캐러셀 적용)

--- src/main.jsx
+++ src/main.jsx
@@ -2,6 +2,8 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
+import Category from './components/common/Category/Category.jsx'
+import WeatherCard from "./components/common/WeatherCard/WeatherCard.jsx";
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
--- src/pages/Main.jsx
+++ src/pages/Main.jsx
@@ -1,12 +1,16 @@
 import React from 'react';
 import ChatAiInfo from '../components/feature/Main/ChatAiInfo';
+import WeatherCardCategory from "../components/feature/Main/WeatherCard_Category.jsx";
 import styles from "./Main.module.css";
+import MainCardNews from "../components/feature/Main/CardNewsCarousel.jsx";
 
 const Main = () => {
     return (
         <div className={styles.wrapper}>
+            <WeatherCardCategory />
             <ChatAiInfo />
-            main page
+            <MainCardNews />
+            main page Cade News
         </div>
     );
 };
